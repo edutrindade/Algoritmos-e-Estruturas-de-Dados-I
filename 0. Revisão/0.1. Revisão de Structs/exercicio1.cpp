7186237848b87b571457f5eb7158ef1ecfb0651b
Erick Sena <ericksena621@gmail.com>
2022-08-21 22:08:16 -0300

    refactor: extract methods

--- 0. Revisão/0.1. Revisão de Structs/exercicio1.cpp
+++ 0. Revisão/0.1. Revisão de Structs/exercicio1.cpp
@@ -4,66 +4,86 @@
 
 using namespace std;
 
-typedef struct Banco {
+typedef struct Banco
+{
     int num;
     char titular[35];
     float saldo;
 };
 
-void preencheContas(Banco conta[]) {
-    for (int i=0; i<NUM_CONTAS; i++) {
-        cout << "Número da conta: ";
-        cin >> conta[i].num;
-        cout << "Titular: ";
-        cin >> conta[i].titular;
-        cout << "Saldo: ";
-        cin >> conta[i].saldo;
+Banco cadastraConta()
+{
+    Banco conta;
+    cout << "Número da conta: ";
+    cin >> conta.num;
+    cout << "Titular: ";
+    cin >> conta.titular;
+    cout << "Saldo: ";
+    cin >> conta.saldo;
+    cout << "\n";
+    return conta;
+}
+
+void preencheContas(Banco contas[])
+{
+    for (int i = 0; i < NUM_CONTAS; i++)
+    {
+        contas[i] = cadastraConta();
     }
 }
 
-int encontraMaior(Banco conta[]) {
-    int maior = 0;
-    for (int i=0; i<NUM_CONTAS; i++) {
-        if (conta[i].saldo > conta[maior].saldo) {
-            maior = i;
+Banco encontraMaior(Banco contas[])
+{
+    Banco maior = contas[0];
+    for (int i = 0; i < NUM_CONTAS; i++)
+    {
+        if (contas[i].saldo > maior.saldo)
+        {
+            maior = contas[i];
         }
     }
     return maior;
 }
 
-int encontraMenor(Banco conta[]) {
-    int menor = 0;
-    for (int i=0; i<NUM_CONTAS; i++) {
-        if (conta[i].saldo < conta[menor].saldo) {
-            menor = i;
+Banco encontraMenor(Banco contas[])
+{
+    Banco menor = contas[0];
+    for (int i = 0; i < NUM_CONTAS; i++)
+    {
+        if (contas[i].saldo < menor.saldo)
+        {
+            menor = contas[i];
         }
     }
     return menor;
 }
 
-void imprimeContas(Banco conta[], int i) {
-    cout << "Conta: " << conta[i].num << endl;
-    cout << "Titular: " << conta[i].titular << endl;
-    cout << "Saldo: R$ " << conta[i].saldo << endl;
+void imprimeConta(Banco conta)
+{
+    cout << "Conta: " << conta.num << endl;
+    cout << "Titular: " << conta.titular << endl;
+    cout << "Saldo: R$ " << conta.saldo << endl;
+    cout << "\n";
 }
- 
-int main() {
+
+int main()
+{
     UINT CPAGE_UTF8 = 65001;
     SetConsoleOutputCP(CPAGE_UTF8);
 
-    Banco conta[NUM_CONTAS];
-    int indice_maior=0, indice_menor=0;
+    Banco contas[NUM_CONTAS];
 
-    preencheContas(conta);
+    preencheContas(contas);
 
-    indice_maior = encontraMaior(conta);
-    indice_menor = encontraMenor(conta);
+    Banco maior_conta, menor_conta;
+    maior_conta = encontraMaior(contas);
+    menor_conta = encontraMenor(contas);
 
     system("cls");
-    
-    cout << "Conta com o maior saldo: \n\n";
-    imprimeContas(conta, indice_maior);
-    cout << "Conta com o menor saldo: \n\n";
-    imprimeContas(conta, indice_menor);
+
+    cout << "Conta com o maior saldo:" << endl;
+    imprimeConta(maior_conta);
+    cout << "Conta com o menor saldo:" << endl;
+    imprimeConta(menor_conta);
     return 0;
 }
--- 0. Revisão/0.2. Revisão de Funções/exercicio1.cpp
+++ 0. Revisão/0.2. Revisão de Funções/exercicio1.cpp
@@ -6,50 +6,68 @@
 
 using namespace std;
 
-typedef struct Aluno {
+typedef struct Aluno
+{
     float nota1, nota2, media;
     char conceito;
 };
 
-float mediaPonderada(float nota1, float nota2) {
-    return (nota1 * P1 + nota2 * P2) / (P1 + P2); 
+float mediaPonderada(float nota1, float nota2)
+{
+    return (nota1 * P1 + nota2 * P2) / (P1 + P2);
 }
 
-char calculaConceito(float media) {
+char calculaConceito(float media)
+{
     if (media >= 9.0)
         return 'A';
     if (media >= 7.0)
         return 'B';
     if (media >= 5.0)
         return 'C';
-    return 'D';    
+    return 'D';
 }
- 
-int main() {
+
+Aluno cadastraAluno()
+{
+    Aluno aluno;
+    cout << "Nota 1: ";
+    cin >> aluno.nota1;
+    cout << "Nota 2: ";
+    cin >> aluno.nota2;
+    aluno.media = mediaPonderada(aluno.nota1, aluno.nota2);
+    aluno.conceito = calculaConceito(aluno.media);
+    return aluno;
+}
+
+void imprimeAluno(Aluno aluno)
+{
+    cout << "Nota 1: " << aluno.nota1 << endl;
+    cout << "Nota 2: " << aluno.nota2 << endl;
+    cout << "Média: " << aluno.media << endl;
+    cout << "Conceito: " << aluno.conceito << endl
+         << endl;
+}
+
+int main()
+{
     UINT CPAGE_UTF8 = 65001;
     SetConsoleOutputCP(CPAGE_UTF8);
-   
+
     Aluno alunos[NUM_ALUNOS];
 
-    for (int i=0; i<NUM_ALUNOS; i++) {
-        cout << "\nALUNO " << i+1 << endl;
-        cout << "Nota 1: " ;
-        cin >> alunos[i].nota1;
-        cout << "Nota 2: ";
-        cin >> alunos[i].nota2;
-        alunos[i].media = mediaPonderada(alunos[i].nota1, alunos[i].nota2);
-        alunos[i].conceito = calculaConceito(alunos[i].media);
+    for (int i = 0; i < NUM_ALUNOS; i++)
+    {
+        cout << "\nALUNO " << i + 1 << endl;
+        alunos[i] = cadastraAluno();
     }
 
     system("cls");
 
-    for (int i=0; i<NUM_ALUNOS; i++) {
-        cout << "ALUNO " << i+1 << endl;
-        cout << "Nota 1: " << alunos[i].nota1 << endl;
-        cout << "Nota 2: " << alunos[i].nota2 << endl;
-        cout << "Média: " << alunos[i].media << endl;
-        cout << "Conceito: " << alunos[i].conceito << endl << endl;
+    for (int i = 0; i < NUM_ALUNOS; i++)
+    {
+        cout << "ALUNO " << i + 1 << endl;
+        imprimeAluno(alunos[i]);
     }
-
     return 0;
 }
